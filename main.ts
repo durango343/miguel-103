7170640a2a323c92d4fa89d8f0662fbbbf2d4691
durango343 <durangom224@gmail.com>
2025-05-23 07:42:51 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,46 @@
+basic.showLeds(`
+    # . . . #
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # #
+    # . . . .
+    # . # # #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # # .
+    # . . . .
+    # # # # .
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # # # # .
+    `)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
